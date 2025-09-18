ecaa3ce2f1eb12bcd2723b9618e6d33d87a3c0c5
Stirling Hostetter <stirlhoss@proton.me>
2025-09-18 12:05:56 -0500

    udpate ico

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -14,7 +14,7 @@ export default createHandler(() => (
         <head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="/tern_berd_trans.ico" />
           {assets}
         </head>
         <body>
